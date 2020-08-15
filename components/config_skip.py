7097dc0ca1fee5ad45ba8fb0d87309b9b5ed914d
WFFTeam <worldfolklorefestivalteam@gmail.com>
2020-08-15 19:22:50 +0200

    Skiplist edited

---
+++ components/config_skip.py
@@ -0,0 +1,22 @@
+skip_hosts = [
+"facebook",
+"instagram",
+"wikipedia",
+"pinterest",
+"amazon",
+"youtube",
+"linkedin",
+"flickr",
+"kupindo",
+"twitter"
+]
+
+skip_ext = [
+".jpg",
+".jpeg",
+".pdf",
+".mp3",
+".mp4",
+".avi",
+".mkv"
+]
