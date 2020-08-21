5d86269d082a3f50f3e45df299ca4978a8953d2e
WFFTeam <worldfolklorefestivalteam@gmail.com>
2020-08-21 03:13:07 +0200

    Ne znam vise

--- Export_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mongoexport -d spadeDB --authenticationDatabase=admin -u glavniAdmin -p 'SifraZnjapetMongo' -c beautifulsoup | mongoimport -h 192.168.1.104:27017 --authenticationDatabase=admin -u glavniAdmin -p 'SifraZnjapetMongo' -d spadeDB -c beautifulsoup --drop
--- Google_repeat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true
-do 
-	python3 mongoSpade_query_google.py
-	echo "Finished query, fetching next in 60 seconds."
-	sleep 30
-done
