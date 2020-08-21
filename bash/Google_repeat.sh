d70a0a6d04a5fdf72964aa8797a60292d8f1bf59
WFFTeam <worldfolklorefestivalteam@gmail.com>
2020-08-21 04:53:32 +0200

    Bash conditional run

--- bash/Google_repeat.sh
+++ bash/Google_repeat.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+process="python3"
+cd /home/spider/spade_mongo/
 while true
-do 
-	python3 mongoSpade_query_google.py
-	echo "Finished query, fetching next in 60 seconds."
-	sleep 30
+do
+        if pgrep "$process" >/dev/null
+        then
+            echo "$process is running, exiting..."
+            exit
+        else
+            echo "Running SPADE search and scrape"
+            python3 mongoSpade_query_google.py
+            echo "Finished query, fetching next in 60 seconds."
+            sleep 30
+        fi
 done
+
